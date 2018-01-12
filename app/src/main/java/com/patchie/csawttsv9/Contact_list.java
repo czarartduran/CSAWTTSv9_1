e148d1d491da4154df707841b1e481974b3c11b3
jibehjibeh <moorefred282@gmail.com>
2018-01-12 20:19:58 +0800

    Call_activity with dial... 1/12/18 with add contact...

--- app/src/main/java/com/patchie/csawttsv9/Contact_list.java
+++ app/src/main/java/com/patchie/csawttsv9/Contact_list.java
@@ -2,6 +2,12 @@ package com.patchie.csawttsv9;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.content.Intent;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+
+
 
 public class Contact_list extends AppCompatActivity {
 
@@ -9,5 +15,27 @@ public class Contact_list extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contact_list);
+        Button buttviewC = (Button) findViewById(R.id.viewC);
+        Button buttaddC = (Button) findViewById(R.id.addC);
+        buttviewC.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent myIntent = new Intent (Contact_list.this,View_contacts.class);
+                startActivity(myIntent);
+
+            }
+        });
+
+        buttaddC.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent myIntent = new Intent (Contact_list.this,Add_contact.class);
+                startActivity(myIntent);
+
+
+            }
+        });
+
     }
 }
+
