8ab5e9fe7628715d5f893f9e0289cbd7ce2e6cfe
jibehjibeh <moorefred282@gmail.com>
2018-01-12 21:26:39 +0800

    Call_activity with dial... 1/12/18 with add contact...

--- app/src/main/java/com/patchie/csawttsv9/CallActivity.java
+++ app/src/main/java/com/patchie/csawttsv9/CallActivity.java
@@ -15,7 +15,7 @@ public class CallActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_call);
 
         Button buttdial = (Button) findViewById(R.id.dialbtn);
         Button buttsc = (Button) findViewById(R.id.scbtn);
