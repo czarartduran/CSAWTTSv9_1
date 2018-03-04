4557b05e2e3a07b1de5c2484102145ed7548cc42
Czar Art Z. Duran <czarartduran@ymail.com>
2018-03-04 14:02:09 +0800

    fixing emergency module

--- app/src/main/java/com/patchie/csawttsv9/EmergencyActivity.java
+++ app/src/main/java/com/patchie/csawttsv9/EmergencyActivity.java
@@ -9,6 +9,9 @@ import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
+import android.hardware.usb.UsbDevice;
+import android.hardware.usb.UsbDeviceConnection;
+import android.hardware.usb.UsbManager;
 import android.net.Uri;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
@@ -21,6 +24,13 @@ import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
+
+import com.felhr.usbserial.UsbSerialDevice;
+import com.felhr.usbserial.UsbSerialInterface;
+
+import java.io.UnsupportedEncodingException;
+import java.util.HashMap;
+import java.util.Map;
 
 public class EmergencyActivity extends AppCompatActivity {
 
@@ -84,6 +94,8 @@ public class EmergencyActivity extends AppCompatActivity {
             public void afterTextChanged(Editable s) {
                 if (Etype != -1) {
                     speaker.speak(ContactNumber.getText().toString());
+                }else {
+                    speaker.speakAdd(ContactNumber.getText().toString());
                 }
 
             }
@@ -114,6 +126,9 @@ public class EmergencyActivity extends AppCompatActivity {
         }
 
         UnRegisterSmsIntent();
+
+        StopScanner();
+        UnRegisterArduinoIntent();
     }
 
     @Override
@@ -145,6 +160,9 @@ public class EmergencyActivity extends AppCompatActivity {
             //Initialize here welcome message
             //speaker.speakAdd(getString(R.string.EmergencyWelcome));
         }
+
+        RegisterArduinoIntent();
+        StartScanner();
     }
 
     @Override
@@ -426,4 +444,198 @@ public class EmergencyActivity extends AppCompatActivity {
         }
 
     }
+
+    private void Append(String num) {
+        final String x = num;
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                Log.e("Czar", x);
+                speaker.speak(x);
+                ContactNumber.append(x);
+                //PhoneNumber = editNum.getText().toString();
+            }
+        });
+    }
+
+    private void backspace() {
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                if (ContactNumber.getText().toString().length() >= 1) {
+                    String old = ContactNumber.getText().toString().substring(ContactNumber.getText().length() - 1);
+                    speaker.speak("Deleting " + old);
+                    String newScreen = ContactNumber.getText().toString().substring(0, ContactNumber.getText().toString().length() - 1);
+                    //editNum.setText(newScreen);
+                    ContactNumber.setText("");
+                    ContactNumber.append(newScreen);
+                }
+            }
+        });
+    }
+
+    /*
+    * ARDUINO GLOBAL VARIABLE
+    * */
+    public final String ACTION_USB_PERMISSION = "com.patchie.csawttsv9.USB_PERMISSION";
+    UsbManager usbManager;
+    UsbDevice device;
+    UsbSerialDevice serialPort;
+    UsbDeviceConnection connection;
+
+    private void CallBack(int x) {
+        ArduinoInputConverter aic = new ArduinoInputConverter(this);
+        /*if (x == aic.CONTROL_PREVIOUS()) {
+            PreviousMessage();
+        }
+        if (x == aic.CONTROL_NEXT()) {
+            NextMessage();
+        }
+        if (x == aic.CONTROL_COMPOSE()) {
+            CallComposeActivity();
+        }
+        if (x == aic.CONTROL_REPLY()) {
+            replyButtonOnClickEvent();
+        }*/
+
+        //appending]
+        if (aic.IsNumber(String.valueOf(x))) {
+            Append(String.valueOf(aic.GetNumber(String.valueOf(x))));
+        }
+        //saving emergency contact number
+        if(x == aic.CONTROL_OK()){
+            SaveEmergencyContact();
+        }
+        if (x == aic.CONTROL_BACKSPACE()) {
+            backspace();
+        }
+        if (x == aic.CONTROL_SEARCH()){
+            SearchRecipient_btn_OnClickEvent();
+        }
+
+        if (x == aic.CONTROL_CANCEL()) {
+            finish();
+        }
+    }
+
+    private void StartScanner() {
+        Log.e("Czar", "StartScanner");
+        HashMap<String, UsbDevice> usbDevices = usbManager.getDeviceList();
+        if (!usbDevices.isEmpty()) {
+            boolean keep = true;
+            for (Map.Entry<String, UsbDevice> entry : usbDevices.entrySet()) {
+                device = entry.getValue();
+                int deviceVID = device.getVendorId();
+                //if (deviceVID == 0x2341)//Arduino Vendor ID
+                if (deviceVID == Integer.valueOf(getString(R.string.VendorID)))//Arduino Vendor ID
+                {
+                    PendingIntent pi = PendingIntent.getBroadcast(this, 0, new Intent(ACTION_USB_PERMISSION), 0);
+                    usbManager.requestPermission(device, pi);
+                    keep = false;
+                } else {
+                    connection = null;
+                    device = null;
+                }
+
+                if (!keep) {
+                    break;
+                }
+            }
+        } else {
+            Log.e("Czar", "No Usb Devices!");
+        }
+    }
+
+    private void StopScanner() {
+        try {
+            if (serialPort.open() == true) {
+                serialPort.close();
+                Log.e("Czar", "SerialPort is Closed!");
+            }
+        } catch (Exception e) {
+            Log.e("Czar", "No serial port to close");
+        }
+    }
+
+    private void RegisterArduinoIntent() {
+        Log.e("SmsActivity", "Registering instent");
+        usbManager = (UsbManager) getSystemService(this.USB_SERVICE);
+        IntentFilter filter = new IntentFilter();
+        filter.addAction(ACTION_USB_PERMISSION);
+        filter.addAction(UsbManager.ACTION_USB_DEVICE_ATTACHED);
+        filter.addAction(UsbManager.ACTION_USB_DEVICE_DETACHED);
+        registerReceiver(broadcastReceiver, filter);
+    }
+
+    private void UnRegisterArduinoIntent() {
+        Log.e("SmsActivity", "UnRegistering Intent");
+        unregisterReceiver(broadcastReceiver);
+    }
+
+    UsbSerialInterface.UsbReadCallback mCallback = new UsbSerialInterface.UsbReadCallback() {
+        //Defining a Callback which triggers whenever data is read.
+
+        @Override
+        public void onReceivedData(byte[] arg0) {
+            Log.e("Czar", "Called: onReceivedData");
+            String data = null;
+            try {
+                data = new String(arg0, "UTF-8");
+                final String input = data;
+                ArduinoInputConverter aic = new ArduinoInputConverter(getApplicationContext());
+
+                int x = aic.getDecimal(input);
+                CallBack(x);
+
+                /*if (x == aic.CONTROL_PREVIOUS()) {
+                    PreviousMessage();
+                }
+                if (x == aic.CONTROL_NEXT()) {
+                    NextMessage();
+                }
+                if (x == aic.CONTROL_COMPOSE()) {
+                    CallComposeActivity();
+                }
+                if (x == aic.CONTROL_REPLY()) {
+                    replyButtonOnClickEvent();
+                }*/
+            } catch (UnsupportedEncodingException e) {
+                //e.printStackTrace();
+                Log.e("Czar", e.getLocalizedMessage());
+            }
+        }
+    };
+
+    private final BroadcastReceiver broadcastReceiver = new BroadcastReceiver() { //Broadcast Receiver to automatically start and stop the Serial connection.
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            Log.e("Czar", "Called: BroadcastReceiver");
+            if (intent.getAction().equals(ACTION_USB_PERMISSION)) {
+                boolean granted = intent.getExtras().getBoolean(UsbManager.EXTRA_PERMISSION_GRANTED);
+                if (granted) {
+                    connection = usbManager.openDevice(device);
+                    serialPort = UsbSerialDevice.createUsbSerialDevice(device, connection);
+                    if (serialPort != null) {
+                        if (serialPort.open()) {
+                            //Set Serial Connection Parameters.
+                            serialPort.setBaudRate(9600);
+                            serialPort.setDataBits(UsbSerialInterface.DATA_BITS_8);
+                            serialPort.setStopBits(UsbSerialInterface.STOP_BITS_1);
+                            serialPort.setParity(UsbSerialInterface.PARITY_NONE);
+                            serialPort.setFlowControl(UsbSerialInterface.FLOW_CONTROL_OFF);
+                            serialPort.read(mCallback);
+                            Log.e("Czar", "SerialPort Opened!");
+
+                        } else {
+                            Log.e("Czar SERIAL", "PORT NOT OPEN");
+                        }
+                    } else {
+                        Log.e("Czar SERIAL", "PORT IS NULL");
+                    }
+                } else {
+                    Log.e("Czar SERIAL", "PERMISSION NOT GRANTED");
+                }
+            }
+        }
+    };
 }
